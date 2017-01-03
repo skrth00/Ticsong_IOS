0830820096ee5882881cb36ad59e27abdc230582
전한경 <Jeon@jeonhangyeong-ui-MacBook-Pro.local>
2017-01-03 17:16:14 +0900

    add alert

--- TicSong_Media/GameController.swift
+++ TicSong_Media/GameController.swift
@@ -2,7 +2,7 @@
 //  GameController.swift
 //  TicSong_Media
 //
-//  Created by 윤민섭 on 2017. 1. 2..
+//  Created by 윤민섭X전한경on 2017. 1. 2..
 //  Copyright © 2017년 yoon. All rights reserved.
 //
 
@@ -10,14 +10,14 @@
 /* 2017.01.03
  1. 노래 리스트업 10곡 선정 V
  2. 점수 기능 (3번의 목숨이 있으며 1번에 맞추면 100점 2번에 맞추면 60점 3번에 맞추면 30점) V
- 3. 아이템 (1. 3초 재생 2. 타이틀 앞 글자 제공 3. 목숨 늘려주기)
+ 3. 아이템 (1. 3초 재생 2. 타이틀 앞 글자 제공 3. 목숨 늘려주기) - 보류
  4. 노래 맞추면 노래 재생 (stop 도 생김) + widget visible /invisible 가능 하게
- 5. 라운드 개념 넣기(5곡이 한 라운드로 가정하고 라운드 중간에 종료시 점수는 무효 / 모든 점수를 합산해서 ResultController로 전송)
- 6. ResultController 로 넘어갈 수 있게 prepareSegue 준비
- 7. ResultController에서 unwindSegue로 값 받아서 서버로 전송하기
- 8. MainController에서 서버에서 불러온 현재의 exp와 level , item 현황 등 을 제공한다.
- 9. 소셜로그인 카카오톡 추가
- 10. 랭킹 테이블뷰 생성하고 서버에서 불러와서 뿌리기
+ 5. 라운드 개념 넣기(5곡이 한 라운드로 가정하고 라운드 중간에 종료시 점수는 무효 / 모든 점수를 합산해서 ResultController로 전송) - 보류
+ 6. ResultController 로 넘어갈 수 있게 prepareSegue 준비 - 다이얼로그 가능성 제기
+ 7. ResultController에서 unwindSegue로 값 받아서 서버로 전송하기 - 보류
+ 8. MainController에서 서버에서 불러온 현재의 exp와 level , item 현황 등 을 제공한다. - 보류
+ 9. 소셜로그인 카카오톡 추가 - 보류
+ 10. 랭킹 테이블뷰 생성하고 서버에서 불러와서 뿌리기 - 보류
  */
 
 import UIKit
@@ -25,9 +25,14 @@ import AVFoundation
 class GameController: UIViewController , AVAudioPlayerDelegate {
     
     
-    // MARK : 멤버 필드
+    // MARK: 멤버 필드
 
+    @IBOutlet weak var stageLabel: UILabel!
+    
     @IBOutlet weak var answer: UITextField!
+    
+    
+    
     
     var audioPlayer:AVAudioPlayer = AVAudioPlayer()
     
@@ -35,27 +40,23 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
     var timer = Timer() // 노래 시간 설정시 사용
     var musicSec = 0
     var startTime : Double!
-    
-    
-    //var arraySong : [String] = ["270052873","287320848","18560800","285714919","17179509","200018532","73847634","196942610","261595798","266565177"]
-    //var arrayTitle : [String] = ["야생화","숨","사랑한후에","꿈","눈의꽃","해줄수없는일","안녕사랑아","동경","화신","나를넘는다"]
-    
     var roundList:[(title:String,song:String,start:Int)] = []
     
-    //var tempSong : Int = 0
     var hintMode: Int = 0 // 0 : 일반 재생 1 : 힌트 재생
     
     var score : Int = 0
     var life : Int = 3
+    var stage : Int = 0
     
     var item : Int = 3
-    var stage : Int = 1
     
-    
-    //MARK : 생명주기
+    //MARK: 생명주기
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        stageLabel.text = "STAGE \(stage+1)"
         
         print(roundList)
         setting(music: roundList[stage].song)
@@ -65,35 +66,13 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // 노래를 설정하는 함수
     
-    func setting(music: String){
-        do {
-            
-
-            //무음에서도 들리게 해주는 부분!! 나중에 정리하면 될거 같아요 민섭님
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            
-            setSong(name: music, time: 100)
-            let fileURL = NSURL(string:url)
-            let soundData = NSData(contentsOf:fileURL! as URL)
-            self.audioPlayer = try AVAudioPlayer(data: soundData! as Data)
-            audioPlayer.prepareToPlay()
-            audioPlayer.volume = 1.0
-            audioPlayer.delegate = self
-            audioPlayer.currentTime = startTime
-        } catch {
-            print(error)
-            print("Error getting the audio file")
-        }
-    }
+   
     
     
 
   
-   
+   //MARK: 각종 버튼
 
     @IBAction func Play(_ sender: UIButton) {
         hintMode = 0
@@ -113,9 +92,8 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         
     }
     
-    @IBAction func Stop(_ sender: UIButton) {
-        audioPlayer.stop()
-    }
+ 
+   
     
     @IBAction func Check(_ sender: UIButton) {
       
@@ -132,12 +110,6 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         else{score += 0}
         
         
-        
-        //다음 노래 준비
-        if(stage < roundList.count){
-            setting(music: roundList[stage].song)
-        }
-        
         }else if answer.text != roundList[stage].title{
             showToast("틀렸습니다!")
         
@@ -149,11 +121,6 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         
             nextStageInit()
             score += 0
-            
-            if(stage < roundList.count){
-                setting(music: roundList[stage].song)
-                print("life 0 되서 다음 스테이지 이동")
-            }
             
             
         }
@@ -175,24 +142,33 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         score += 0
         
         
-        if(stage < roundList.count){
-            setting(music: roundList[stage].song)
-            showToast("다음 스테이지 넘어감")
-        }
-        
-        
     }
-    
-    func nextStageInit(){
-    
-        stage += 1
-        life = 3
-    }
-    
-    
     
    
-    // 노래 재생 시간 설정 ( music_sec 가 초를 나타낸다 )
+    
+   //MARK: 노래 재생 설정
+    
+    func setting(music: String){
+        do {
+            //무음에서도 들리게 해주는 부분!! 나중에 정리하면 될거 같아요 민섭님
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            setSong(name: music, time: 100)
+            let fileURL = NSURL(string:url)
+            let soundData = NSData(contentsOf:fileURL! as URL)
+            self.audioPlayer = try AVAudioPlayer(data: soundData! as Data)
+            audioPlayer.prepareToPlay()
+            audioPlayer.volume = 1.0
+            audioPlayer.delegate = self
+            audioPlayer.currentTime = startTime
+        } catch {
+            print(error)
+            print("Error getting the audio file")
+        }
+    }
+    
+   // 노래 재생 시간 설정 ( musicSec가 초를 나타낸다 )
     
     func counter() {
         musicSec += 1
@@ -216,7 +192,45 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         startTime = time
     }
     
-    // 토스트 띄우는 함수
+   
+    
+    // 노래 재생 하는 함수
+    
+    func playMusic(){
+        musicSec = 0
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameController.counter), userInfo: nil, repeats:true)
+        audioPlayer.play()
+    }
+    
+    func nextStageInit(){
+        stage += 1
+        life = 3
+        
+        alertTest(songTitle: roundList[stage-1].title)
+        audioPlayer.play()
+    }
+    
+    func nextSong(){
+        
+        audioPlayer.stop()
+        
+        
+        //다음 노래 준비
+        if(stage < roundList.count){
+            
+            stageLabel.text = "STAGE \(stage+1)"
+            setting(music: roundList[stage].song)
+            print("다음 노래 준비!")
+        }else{
+            //모든 스테이지 종료 시 일단은 라벨 제거함
+            //Alert창 띄워서 결과보여주고 확인누르면 메인으로 돌아가게 만들까?
+            stageLabel.isHidden = true
+            resultAlert(score: score)
+            
+        }
+    }
+    
+    // MARK: 여러가지 Alert
     
     func showToast(_ msg:String) {
         let toast = UIAlertController()
@@ -230,13 +244,59 @@ class GameController: UIViewController , AVAudioPlayerDelegate {
         }
     }
     
-    // 노래 재생 하는 함수
-    
-    func playMusic(){
-        musicSec = 0
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameController.counter), userInfo: nil, repeats:true)
-        audioPlayer.play()
+    func alertTest(songTitle:String){
+        
+        let alertView = UIAlertController(title: songTitle, message: "소울트리~", preferredStyle: .alert)
+        
+//        let image = UIImage(named: "album")
+//        let realiamge = UIImageView(image: image)
+//        realiamge.frame = CGRect(x: 0, y: 100, width: 250, height: 250)
+//        
+//        alertView.view.addSubview(realiamge)
+        
+        
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+            alertView.dismiss(animated: true, completion: nil)
+            
+            self.nextSong()
+        })
+        alertView.addAction(action)
+        
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindowLevelAlert + 1
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alertView, animated: true, completion: nil)
     }
+    
+  
+    
+    
+    func resultAlert(score:Int){
+        
+        let alertView = UIAlertController(title: "RESULT", message: "당신의 점수는 \(score)입니다!", preferredStyle: .alert)
+        
+//        let image = UIImage(named: "album")
+//        let realiamge = UIImageView(image: image)
+//        realiamge.frame = CGRect(x: 0, y: 100, width: 250, height: 250)
+//        
+//        alertView.view.addSubview(realiamge)
+        
+        
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+            alertView.dismiss(animated: true, completion: nil)
+            self.dismiss(animated: true, completion: nil)
+        })
+        alertView.addAction(action)
+        
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindowLevelAlert + 1
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alertView, animated: true, completion: nil)
+    }
+    
+    
     
   
 
